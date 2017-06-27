a976dc6cdb3038e118b60b3eff44263f62170088
Matt Bogenhagen <mbogenhagen@ebates.com>
2017-06-27 14:03:11 -0500

    Add some more to the tests

--- test/test.js
+++ test/test.js
@@ -9,38 +9,33 @@ describe('Dice Roll', function () {
   it('Normal dice roll (No params)', function () {
     let roll = commands.roll()
     expect(roll.faces).to.equal(20)
-    expect(roll.sum).to.be.above(0)
-    expect(roll.sum).to.be.below(21)
+    expect(roll.sum).to.be.within(1, 20)
     expect(roll.message).to.equal('Rolled')
   })
   it('Normal dice roll out of 6', function () {
     let roll = commands.roll(6)
     expect(roll.faces).to.equal(6)
-    expect(roll.sum).to.be.above(0)
-    expect(roll.sum).to.be.below(7)
+    expect(roll.sum).to.be.within(1, 6)
     expect(roll.message).to.equal('Rolled')
   })
   it('2 dice rolls out of 20', function () {
     let roll = commands.roll('2d20')
     expect(roll.faces).to.equal(20)
     expect(roll.results).to.be.an('array')
-    expect(roll.sum).to.be.above(0)
-    expect(roll.sum).to.be.below(41)
+    expect(roll.sum).to.be.within(1, 50)
     expect(roll.message).to.equal('Rolled')
   })
   it('Normal dice roll with custom message', function () {
     let roll = commands.roll('# Attack')
     expect(roll.faces).to.equal(20)
-    expect(roll.sum).to.be.above(0)
-    expect(roll.sum).to.be.below(21)
+    expect(roll.sum).to.be.within(1, 20)
     expect(roll.message).to.equal('Attack')
   })
   it('5 dice rolls out of 6 with custom message', function () {
     let roll = commands.roll('5d6 # Blerg')
     expect(roll.faces).to.equal(6)
     expect(roll.results).to.be.an('array')
-    expect(roll.sum).to.be.above(0)
-    expect(roll.sum).to.be.below(31)
+    expect(roll.sum).to.be.within(1, 30)
     expect(roll.message).to.equal('Blerg')
   })
 })
@@ -51,22 +46,28 @@ describe('Meme', async function () {
   it('Random Meme', async function () {
     let meme = await commands.meme.get()
     expect(meme.title).to.be.ok
+    expect(meme.title).to.be.a('string')
     expect(meme.image).to.be.ok
+    expect(meme.image).to.be.a('string')
     expect(meme.nsfw).to.not.be.ok
     expect(meme.error).to.not.be.ok
   })
   it('Meme from pcmasterrace', async function () {
     let meme = await commands.meme.get('pcmasterrace')
     expect(meme.title).to.be.ok
+    expect(meme.title).to.be.a('string')
     expect(meme.image).to.be.ok
+    expect(meme.image).to.be.a('string')
     expect(meme.nsfw).to.not.be.ok
     expect(meme.error).to.not.be.ok
   })
   it('NSFW meme', async function () {
     let meme = await commands.meme.get('hentai')
     expect(meme.title).to.be.ok
+    expect(meme.title).to.be.a('string')
     expect(meme.image).to.be.ok
-    expect(meme.nsfw).to.be.ok
+    expect(meme.image).to.be.a('string')
+    expect(meme.nsfw).to.be.true
     expect(meme.error).to.not.be.ok
   })
   it('Meme not found', async function () {
@@ -75,5 +76,6 @@ describe('Meme', async function () {
     expect(meme.image).to.not.be.ok
     expect(meme.nsfw).to.not.be.ok
     expect(meme.error).to.be.ok
+    expect(meme.error).to.be.a('string')
   })
 })
